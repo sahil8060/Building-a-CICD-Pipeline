608519e985e79bfca20e519f0af802fb94ad58cf
sahil8060 <72242460+sahil8060@users.noreply.github.com>
2021-11-12 10:38:33 +0530

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,1 +1,1 @@
-az webapp up -n mywebapp-service -g goyalsahil007_rg_Linux_centralus -p goyalsahil007aspLinuxcentralus
+az webapp up -n mywebapp-service -g rg_Linux_centralus -p asp_Linux_centralus --sku F1
